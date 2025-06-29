0a761dcfd242f55f9e5cced155200712aa67dd24
constantly-dev <rntqhdl10@naver.com>
2025-06-30 07:17:25 +0900

    setting: design-system 예시 코드 추가

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,7 +1,15 @@
+import { Button } from '@pinback/design-system';
 import './App.css';
 
 function App() {
-  return <div>핀백 화이팅</div>;
+  return (
+    <>
+      <div>
+        핀백 화이팅
+        <Button />
+      </div>
+    </>
+  );
 }
 
 export default App;
