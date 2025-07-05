51535b39b018dbc2f1d41853b0e0f66b03b223c8
KIM JEONGMIN <163228804+jjangminii@users.noreply.github.com>
2025-07-06 03:46:34 +0900

    Setting(project): axios query setup (#12)
    * setting : landing queryClient 설정
    
    * setting : client  Tanstack query 설정
    
    * Stting : client Axios 설정 (instance)
    
    * Setting : landing Axios 설정 (instance)
    
    * refactor : 안쓰는 코드 삭제
    
    * refactor : 안쓰는 코드 정리
    
    * chore: 중복코드 제거

--- apps/landing/src/main.tsx
+++ apps/landing/src/main.tsx
@@ -1,9 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { queryClient } from "./shared/utils/queryClient";
+
 import App from "./App.tsx";
+import "./index.css";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </StrictMode>
 );
