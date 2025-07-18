f73064216881576be7783920ac7b638166e05434
jllee000 <jllee000@naver.com>
2025-07-19 00:15:28 +0900

    feat: 대시보드 랜딩

--- apps/client/src/shared/components/ui/layout/Header.tsx
+++ apps/client/src/shared/components/ui/layout/Header.tsx
@@ -9,7 +9,12 @@ const Header = ({ bg = 'bg-background' }: HeaderProps) => {
     <header
       className={`fixed left-1/2 top-0 z-50 flex h-[7.4rem] w-[1440px] -translate-x-1/2 items-center px-[6rem] pb-[1.7rem] pt-[2.5rem] ${bg}`}
     >
-      <img src={logo} className="h-[3.2rem] w-[12.4rem]" alt="로고" />
+      <img
+        src={logo}
+        className="h-[3.2rem] w-[12.4rem]"
+        alt="로고"
+        onClick={() => (window.location.href = 'https://www.pinback.today/')}
+      />
     </header>
   );
 };
